30d28646015d95d8857d2db9f50efa41aad1ee0f
Patrik Smeds <patrik.smeds@gmail.com>
2018-09-05 15:48:15 +0200

    test

---
+++ bio/amplicon_mapping/wrapper.py
@@ -0,0 +1,33 @@
+__author__ = "Patrik Smeds"
+__copyright__ = "Copyright 2017, Patrik Smeds"
+__email__ = "patrik.smeds@scilifelab.uu.se"
+__license__ = "MIT"
+
+from snakemake.shell import shell
+
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+genome_ref = snakemake.params.get("genome_ref")
+
+if genome_ref is None:
+    raise ValueError("A genome reference must be provided")
+
+design_file = snakemake.params.get("design_file")
+
+if design_file is None:
+    raise ValueError("A design file, contain amplicon in bed format, must be provded")
+
+shell(
+ "AmpliconMapping.sh " +
+ "-T MapReadToAmpliconsIlluminaReadPair " +
+ "-R " + genome_ref + " " +
+ "-I {snakemake.input} " +
+ "-o {snakemake.output.bed} " +
+ "-fragments " + design_file + " " +
+ "-ampAnReads {snakemake.output.bam} " +
+ "-U ALL " +
+ "-nonunique " +
+ "-allowPotentiallyMisencodedQuals " +
+ "--downsample_to_coverage 90000 " +
+ "-molBarCode 0 " +
+ "{log}")
---
+++ bio/jsnpmania/wrapper.py
@@ -0,0 +1,24 @@
+__author__ = "Patrik Smeds"
+__copyright__ = "Copyright 2017, Patrik Smeds"
+__email__ = "patrik.smeds@scilifelab.uu.se"
+__license__ = "MIT"
+
+from snakemake.shell import shell
+
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+path_jsnpmania_header = snakemake.params.get("path_jsnpmania_header")
+
+if path_jsnpmania_header is None:
+    raise ValueError("path to jsnpmania header cannot be empty")
+
+shell(
+ "samtools reheader " + path_jsnpmania_header + " {snakemake.input} | " +
+ "samtools view /dev/stdin | " +
+ "JSNPmania.v2.sh -i /dev/stdin " + "-o {snakemake.output.variations} " +
+    "-oi {snakemake.output.insertions} " +
+    "-od {snakemake.output.deletions} " +
+    "-r {snakemake.params.ref_file} " +
+    " {snakemake.params.flags} " +
+    "{log}"
+)
